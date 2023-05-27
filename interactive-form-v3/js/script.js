fc5a1d8aa7d3636a72d7868b2d89f17111a9ba3d
Suzanna Mitchell <suziequ33@gmail.com>
2023-05-26 23:01:42 -0500

    updating and working on project code

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -16,7 +16,7 @@ nameFocus.focus();
 */
 const titleJob = document.getElementById('title'); 
 const otherJobRole = document.getElementById('other-job-role');
-const titleOption = document.querySelectorAll('title option');
+//const titleOption = document.querySelectorAll('title option');
 otherJobRole.style.display = 'none';
 
 titleJob.addEventListener('change', e => {
@@ -59,7 +59,7 @@ shirtDesign.addEventListener('change', e => {
 //get the name chaeckbox within fieldset element
 // gets the total cost of element in activites-cost
 //found example for parseInt in StingManipulation with JavaScript to turn the string into a number value.
-//i had trouble with i found the .replace in notes 2-Regular Expressions in JavaScript and also examples on stackoverflow.
+//i had trouble with i found the .replace in notes 2-Regular Expressions in JavaScript(Flags) and also examples on stackoverflow.
 //update the total cost .
 const activitesFieldset = document.getElementById('activities');
 const checkBox = activitesFieldset.querySelectorAll('input[type="checkbox"]');
@@ -81,6 +81,85 @@ if(boxChecked.type === 'checkbox') {
 totalCostElem.textContent = 'Total: $' + totalCost;
     }
 }); 
+/**Payment infor*/
+//get the payment method dropdown 'payment'
+//gets all the payment sections. example of selectorAll in JavaScript and the DOM.also on mdn web docs
+//.forEach loop checks each payment section and hides then by setting display property to 'none'.
+//sectionPayment shows the selected payment sections. 
+//the last .forEach loop hides all payment except for credit-card on page load.
+//I found example of this .forEach loop on w3school.com
+const payMethod = document.getElementById('payment');
+const paymentSections = document.querySelectorAll('.credit-card, .paypal, .bitcoin');
+document.getElementById('credit-card').style.display = 'block';
+
+payMethod.addEventListener('change', e => {
+    const selectPayment = e.target.value;
+
+    paymentSections.forEach(section => {
+        section.style.display = 'none';
+    });
+
+    const sectionPayment = document.getElementById(selectPayment);
+    sectionPayment.style.display ='block';
+});
+
+paymentSections.forEach(section => {
+    if (section.id !== 'credit-card') {
+        section.style.display = 'none';
+    }
+});
+/**Form Validation */
+// I found a example of form submit event at javasript.plainenglish 
+//e.preventDefault DOM Dctiption By Example(RSVP Checkbox)
+//.trim() Sting Manipulation with JavaSript(Modification Methods)
+//I found an example of how to Validate and they used .classList. I also looked up .classList @ w3schools.com
+//for (error-border) is on developer.mozilla for border error and also on stackoverflow.
+//I had trouble with 'name-hint'. I was trying just name element. Or querySeletALL. Until you see in index.html name-hint
+
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    var isValid = true;
+
+    var nameInput = document.getElementById('name');
+    if (nameInput.value.trim() === '') {
+        isValid = false;
+        nameInput.classList.add('error-border');
+        document.getElementById('name-hint').style.display = 'block';
+    } else {
+        nameInput.classList.remove('error-border');
+        document.getElementById('name-hint').style.display = 'none';
+    }
+
+    var emailInput = document.getElementById('email');
+    var emailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailValid.test(emailInput.value.trim())) {
+       isValid = false;
+       emailInput.classList.add('error-border');
+       document.getElementById('email-hint').style.display = 'block';
+    } else {
+        emailInput.classList.remove('error-border');
+        document.getElementById('email-hint').style.display = 'none';
+    }
+
+    var activitiesInput = document.getElementById('activities');
+    var activitiesBox = document.getElementById('activities-box');
+    var checkedActivities = activitiesBox.querySelectorAll('input[type="checkbox"]:checked');
+    if(checkedActivities.length === 0) {
+        isValid = false;
+        activitiesInput.classList.add('error-border');
+        document.getElementById('activities-hint').style.display = 'block';
+    } else {
+        activitiesInput.classList.remove('error-border');
+        document.getElementById(activites-hint).style.display = 'none';
+    }
+    
+
+
+});
 
 
 
